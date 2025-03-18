f8810dfa718a41aaac3e608419bc118c38286fe4
KIT-IT <motokitt@yandex.ru>
2025-03-18 22:28:37 +0300

    add lesson 6

--- lesson_6/outputs.tf
+++ lesson_6/outputs.tf
@@ -6,8 +6,16 @@ output "external_nginx_lb_ip" {
   value = yandex_compute_instance.nginx_lb.network_interface.0.nat_ip_address
 }
 
-output "balance_nginx_lb_ip" {
-  value = format("IP для балансировки: %s/api", yandex_compute_instance.nginx_lb.network_interface.0.nat_ip_address)
+output "round_robin_balance_nginx_lb_ip" {
+  value = format("IP для балансировки: %s/api/round_robin", yandex_compute_instance.nginx_lb.network_interface.0.nat_ip_address)
+}
+
+output "hash_balance_nginx_lb_ip" {
+  value = format("IP для hash балансировки: %s/api/hash", yandex_compute_instance.nginx_lb.network_interface.0.nat_ip_address)
+}
+
+output "wordpress_balance_nginx_lb_ip" {
+  value = format("IP для wordpress балансировки: %s/api/wordpress", yandex_compute_instance.nginx_lb.network_interface.0.nat_ip_address)
 }
 
 output "internal_nginx_backend_1_ip" {
@@ -26,5 +34,11 @@ output "external_nginx_backend_2_ip" {
   value = yandex_compute_instance.nginx_backend_2.network_interface.0.ip_address
 }
 
+output "internal_wordpress_ip" {
+  value = yandex_compute_instance.wordpress.network_interface[0].nat_ip_address
+}
 
+output "external_wordpress_ip" {
+  value = yandex_compute_instance.wordpress.network_interface[0].ip_address
+}
 
